112aec7e79bd50c2393cdf1ef4f8577f670facb4
Nico Kestel <=>
2020-11-11 09:18:34 +0100

    init no agent yet

---
+++ agents/common.py
@@ -0,0 +1,242 @@
+from enum import Enum
+from typing import Optional
+from typing import Callable, Tuple
+import numpy as np
+
+COLUMNS = np.int8(7)  # number of columns on board
+ROWS = np.int(6)  # number of rows on board
+
+BoardPiece = np.int8  # The data type (dtype) of the board
+NO_PLAYER = BoardPiece(0)  # board[i, j] == NO_PLAYER where the position is empty
+PLAYER1 = BoardPiece(1)  # board[i, j] == PLAYER1 where player 1 has a piece
+PLAYER2 = BoardPiece(2)  # board[i, j] == PLAYER2 where player 2 has a piece
+
+PlayerAction = np.int8  # The column to be played
+
+
+class SavedState:
+    pass
+
+
+GenMove = Callable[
+    [np.ndarray, BoardPiece, Optional[SavedState]],  # Arguments for the generate_move function
+    Tuple[PlayerAction, Optional[SavedState]]  # Return type of the generate_move function
+]
+
+
+class GameState(Enum):
+    IS_WIN = 1
+    IS_DRAW = -1
+    STILL_PLAYING = 0
+
+
+def initialize_game_state() -> np.ndarray:
+    """
+    Returns an ndarray, shape (6, 7) and data type (dtype) BoardPiece, initialized to 0 (NO_PLAYER).
+    """
+
+    board = np.tile(NO_PLAYER, (ROWS, COLUMNS)).astype(BoardPiece)
+    return board
+
+
+def pretty_print_board(board: np.ndarray) -> str:
+    """
+    Should return `board` converted to a human readable string representation,
+    to be used when playing or printing diagnostics to the console (stdout). The piece in
+    board[0, 0] should appear in the lower-left. Here's an example output:
+    |==============|
+    |              |
+    |              |
+    |    X X       |
+    |    O X X     |
+    |  O X O O     |
+    |  O O X X     |
+    |==============|
+    |0 1 2 3 4 5 6 |
+    """
+
+    # add header
+    board_str = '|' + COLUMNS * '==' + '|'
+
+    for i in range(ROWS):
+        # left border
+        board_str += '\n|'
+        for j in range(COLUMNS):
+            # player pieces
+            if board[ROWS - i - 1, j] == NO_PLAYER:
+                board_str += '  '
+            elif board[ROWS - i - 1, j] == PLAYER1:
+                board_str += 'X '
+            elif board[ROWS - i - 1, j] == PLAYER2:
+                board_str += 'O '
+        # right border
+        board_str += '|'
+    # bottom separator
+    board_str += '\n|' + COLUMNS * '==' + '|'
+    # footer
+    board_str += '\n|'
+    for j in range(COLUMNS):
+        board_str += str(j) + ' '
+    board_str += '|'
+    return board_str
+
+
+def string_to_board(pp_board: str) -> np.ndarray:
+    """
+    Takes the output of pretty_print_board and turns it back into an ndarray.
+    This is quite useful for debugging, when the agent crashed and you have the last
+    board state as a string.
+    """
+
+    # board to return after board pieces are placed
+    board = initialize_game_state()
+
+    # remove header and footer
+    rows = pp_board.splitlines()
+    rows = rows[1:-2]
+
+    # process row-wise
+    for r in range(ROWS):
+        row = rows[r].replace('|', '')
+        for i in range(COLUMNS):
+            piece = row[i * 2: (i * 2) + 1]
+            if piece == 'O':
+                board[COLUMNS - r - 2, i] = PLAYER2  # -2 correction because r is in 0..5 (not 1..6)
+            elif piece == 'X':
+                board[COLUMNS - r - 2, i] = PLAYER1
+
+    return board
+
+
+def apply_player_action(
+        board: np.ndarray, action: PlayerAction, player: BoardPiece, copy: bool = False
+) -> np.ndarray:
+    """
+    Sets board[i, action] = player, where i is the lowest open row. The modified
+    board is returned. If copy is True, makes a copy of the board before modifying it.
+    """
+
+    # optionally copy board
+    if copy:
+        mod_board = board.copy()
+    else:
+        mod_board = board
+
+    # check desired column
+    col = mod_board[:, action]
+    # if column is not full
+    if col[-1] == NO_PLAYER:
+        mod_board[col.argmin(), action] = player
+
+    return mod_board
+
+
+def connected_four(
+        board: np.ndarray, player: BoardPiece, last_action: Optional[PlayerAction] = None,
+) -> bool:
+    """
+    Returns True if there are four adjacent pieces equal to `player` arranged
+    in either a horizontal, vertical, or diagonal line. Returns False otherwise.
+    If desired, the last action taken (i.e. last column played) can be provided
+    for potential speed optimisation.
+    """
+
+    masked_board = (board == player).astype(BoardPiece)
+    # print('\n', masked_board)
+
+    if last_action:
+        x0 = last_action
+        y0 = board[:, last_action].argmin() - 1  # highest entry with piece
+
+        # horizontal
+        for j in range(COLUMNS - 3):
+            if masked_board[y0, j] and masked_board[y0, j + 1] and masked_board[y0, j + 2] and masked_board[y0, j + 3]:
+                return True
+
+        # vertical
+        for i in range(ROWS - 3):
+            if masked_board[i, x0] and masked_board[i + 1, x0] and masked_board[i + 2, x0] and masked_board[i + 3, x0]:
+                return True
+
+        # check diagonals with count walker
+        # walk along diagonals until non-player piece occurs
+        directions = [(-1, -1), (-1, 1)]  # one for each diagonal
+        for y_step, x_step in directions:
+            count = 0
+            xi = x0
+            yi = y0
+            while xi in range(0, COLUMNS) and yi in range(0, ROWS):
+                if masked_board[yi, xi]:
+                    yi += y_step
+                    xi += x_step
+                    count += 1
+                else:
+                    yi += y_step
+                    xi += x_step
+                    continue
+
+                if count >= 4:
+                    return True
+
+            yi = y0 - y_step
+            xi = x0 - x_step
+            while xi in range(0, COLUMNS) and yi in range(0, ROWS):
+                if masked_board[yi, xi]:
+                    count += 1
+                else:
+                    break
+
+                yi -= y_step
+                xi -= x_step
+
+                if count >= 4:
+                    return True
+        return False
+
+    else:
+        # horizontal
+        for i in range(ROWS):
+            for j in range(COLUMNS - 3):
+                if masked_board[i, j] and masked_board[i, j + 1] and masked_board[i, j + 2] and masked_board[i, j + 3]:
+                    return True
+
+        # vertical
+        for j in range(COLUMNS):
+            for i in range(ROWS - 3):
+                if masked_board[i, j] and masked_board[i + 1, j] and masked_board[i + 2, j] and masked_board[i + 3, j]:
+                    return True
+
+        # / diagonals
+        for i in range(ROWS - 3):
+            for j in range(3, COLUMNS):
+                if masked_board[i, j] and masked_board[i + 1, j - 1] and masked_board[i + 2, j - 2] \
+                        and masked_board[i + 3, j - 3]:
+                    return True
+
+        # \ diagonals
+        for i in range(3, ROWS):
+            for j in range(3, COLUMNS):
+                if masked_board[i, j] and masked_board[i - 1, j - 1] and masked_board[i - 2, j - 2] \
+                        and masked_board[i - 3, j - 3]:
+                    return True
+
+    return False
+
+
+def check_end_state(
+        board: np.ndarray, player: BoardPiece, last_action: Optional[PlayerAction] = None,
+) -> GameState:
+    """
+    Returns the current game state for the current `player`, i.e. has their last
+    action won (GameState.IS_WIN) or drawn (GameState.IS_DRAW) the game,
+    or is play still on-going (GameState.STILL_PLAYING)?
+    """
+
+    if connected_four(board, player, last_action):
+        return GameState.IS_WIN
+
+    # upper row needs to be full to be drawn
+    if np.all(board[-1, :]):
+        return GameState.IS_DRAW
+
+    return GameState.STILL_PLAYING
---
+++ tests/test_common.py
@@ -0,0 +1,245 @@
+from agents.common import *
+import numpy as np
+
+
+def test_initialize_game_state():
+    board = initialize_game_state()
+
+    assert isinstance(board, np.ndarray)
+    assert board.shape == (ROWS, COLUMNS)
+    assert board.dtype == BoardPiece
+    assert board.all() == NO_PLAYER
+
+
+def test_pretty_print_board():
+    board = initialize_game_state()
+    board[0, 1] = PLAYER2
+    board[1, 1] = PLAYER2
+
+    board[0, 2] = PLAYER2
+    board[1, 2] = PLAYER1
+    board[2, 2] = PLAYER2
+    board[3, 2] = PLAYER1
+
+    board[0, 3] = PLAYER1
+    board[1, 3] = PLAYER2
+    board[2, 3] = PLAYER1
+    board[3, 3] = PLAYER1
+
+    board[0, 4] = PLAYER1
+    board[1, 4] = PLAYER2
+    board[2, 4] = PLAYER1
+
+    cmp_board = "|==============|\n" \
+                "|              |\n" \
+                "|              |\n" \
+                "|    X X       |\n" \
+                "|    O X X     |\n" \
+                "|  O X O O     |\n" \
+                "|  O O X X     |\n" \
+                "|==============|\n" \
+                "|0 1 2 3 4 5 6 |"
+
+    assert pretty_print_board(board) == cmp_board
+
+
+def test_string_to_board():
+    input_board = "|==============|\n" \
+                  "|              |\n" \
+                  "|              |\n" \
+                  "|    X X       |\n" \
+                  "|    O X X     |\n" \
+                  "|  O X O O     |\n" \
+                  "|  O O X X     |\n" \
+                  "|==============|\n" \
+                  "|0 1 2 3 4 5 6 |"
+
+    cmp_board = initialize_game_state()
+    cmp_board[0, 1] = PLAYER2
+    cmp_board[1, 1] = PLAYER2
+
+    cmp_board[0, 2] = PLAYER2
+    cmp_board[1, 2] = PLAYER1
+    cmp_board[2, 2] = PLAYER2
+    cmp_board[3, 2] = PLAYER1
+
+    cmp_board[0, 3] = PLAYER1
+    cmp_board[1, 3] = PLAYER2
+    cmp_board[2, 3] = PLAYER1
+    cmp_board[3, 3] = PLAYER1
+
+    cmp_board[0, 4] = PLAYER1
+    cmp_board[1, 4] = PLAYER2
+    cmp_board[2, 4] = PLAYER1
+
+    assert string_to_board(input_board).shape == (ROWS, COLUMNS)
+    assert np.allclose(string_to_board(input_board), cmp_board)
+
+
+def test_apply_player_action():
+    cmp_board = initialize_game_state()
+    cmp_board[0, 0] = PLAYER2
+    cmp_board[0, 1] = PLAYER1
+    cmp_board[0, 2] = PLAYER2
+    cmp_board[1, 1] = PLAYER1
+    cmp_board[2, 1] = PLAYER2
+    cmp_board[3, 1] = PLAYER2
+    cmp_board[4, 1] = PLAYER2
+    cmp_board[5, 1] = PLAYER2
+
+    board = initialize_game_state()
+    board = apply_player_action(board=board, action=PlayerAction(0), player=PLAYER2)
+    board = apply_player_action(board=board, action=PlayerAction(1), player=PLAYER1)
+    board = apply_player_action(board=board, action=PlayerAction(2), player=PLAYER2)
+    board = apply_player_action(board=board, action=PlayerAction(1), player=PLAYER1)
+    board = apply_player_action(board=board, action=PlayerAction(1), player=PLAYER2)
+    board = apply_player_action(board=board, action=PlayerAction(1), player=PLAYER2)
+    board = apply_player_action(board=board, action=PlayerAction(1), player=PLAYER2)
+    board = apply_player_action(board=board, action=PlayerAction(1), player=PLAYER2)
+    # test column capacity
+    board = apply_player_action(board=board, action=PlayerAction(1), player=PLAYER2)
+
+    assert np.allclose(board, cmp_board)
+
+    # test copy
+    copy_board = apply_player_action(board=board, action=PlayerAction(6), player=PLAYER1,
+                                     copy=True)
+
+    assert not np.allclose(board, copy_board)
+
+
+def test_connected_four():
+    board1 = "|==============|\n" \
+             "|              |\n" \
+             "|              |\n" \
+             "|    X X       |\n" \
+             "|    O X X     |\n" \
+             "|  O X O O     |\n" \
+             "|  O O X X     |\n" \
+             "|==============|\n" \
+             "|0 1 2 3 4 5 6 |"
+
+    board2 = "|==============|\n" \
+             "|              |\n" \
+             "|              |\n" \
+             "|    X X       |\n" \
+             "|    X X X     |\n" \
+             "|  O X O O     |\n" \
+             "|  O X X X     |\n" \
+             "|==============|\n" \
+             "|0 1 2 3 4 5 6 |"
+
+    board3 = "|==============|\n" \
+             "|              |\n" \
+             "|              |\n" \
+             "|  O X X       |\n" \
+             "|  X O X X     |\n" \
+             "|  O X O O     |\n" \
+             "|  O O X O     |\n" \
+             "|==============|\n" \
+             "|0 1 2 3 4 5 6 |"
+
+    board4 = "|==============|\n" \
+             "|              |\n" \
+             "|              |\n" \
+             "|    X X     X |\n" \
+             "|    O X X X O |\n" \
+             "|  O X O X O O |\n" \
+             "|  O O X X O O |\n" \
+             "|==============|\n" \
+             "|0 1 2 3 4 5 6 |"
+
+    board5 = "|==============|\n" \
+             "|              |\n" \
+             "|              |\n" \
+             "|    X X     X |\n" \
+             "|    O X X X O |\n" \
+             "|  O X O X O O |\n" \
+             "|  O O X X X O |\n" \
+             "|==============|\n" \
+             "|0 1 2 3 4 5 6 |"
+
+    board_draw = "|==============|\n" \
+                 "|O O O X O O O |\n" \
+                 "|X X X O X X X |\n" \
+                 "|O O X X X O X |\n" \
+                 "|X X O O X X O |\n" \
+                 "|O O X X O O X |\n" \
+                 "|O O X X O X O |\n" \
+                 "|==============|\n" \
+                 "|0 1 2 3 4 5 6 |"
+
+    board_full = "|==============|\n" \
+                 "|O O X O O O O |\n" \
+                 "|X X X O X X X |\n" \
+                 "|O O X X X O X |\n" \
+                 "|X X O O X X O |\n" \
+                 "|O O X X O O X |\n" \
+                 "|O O X X O X O |\n" \
+                 "|==============|\n" \
+                 "|0 1 2 3 4 5 6 |"
+
+    assert not connected_four(string_to_board(board1), PLAYER1)
+    assert connected_four(string_to_board(board2), PLAYER1)
+    assert connected_four(string_to_board(board3), PLAYER2)
+    assert connected_four(string_to_board(board4), PLAYER1)
+
+    # test last action
+    assert connected_four(string_to_board(board5), PLAYER1, last_action=PlayerAction(5))
+    assert connected_four(string_to_board(board5), PLAYER1, last_action=PlayerAction(2))
+    assert not connected_four(string_to_board(board5), PLAYER1, last_action=PlayerAction(4))
+
+    assert not connected_four(string_to_board(board_draw), PLAYER1)
+    assert not connected_four(string_to_board(board_draw), PLAYER2)
+
+    assert not connected_four(string_to_board(board_full), PLAYER1)
+    assert connected_four(string_to_board(board_full), PLAYER2)
+
+
+def test_check_end_state():
+    board1 = "|==============|\n" \
+             "|              |\n" \
+             "|              |\n" \
+             "|    X X       |\n" \
+             "|    O X X     |\n" \
+             "|  O X O O     |\n" \
+             "|  O O X X     |\n" \
+             "|==============|\n" \
+             "|0 1 2 3 4 5 6 |"
+
+    board2 = "|==============|\n" \
+             "|              |\n" \
+             "|              |\n" \
+             "|    X X       |\n" \
+             "|    X X X     |\n" \
+             "|  O X O O     |\n" \
+             "|  O X X X     |\n" \
+             "|==============|\n" \
+             "|0 1 2 3 4 5 6 |"
+
+    board3 = board2.replace('X', 'T').replace('O', 'X').replace('T', 'O')  # flip Xs and Os
+
+    board_draw = "|==============|\n" \
+                 "|O O O X O O O |\n" \
+                 "|X X X O X X X |\n" \
+                 "|O O X X X O X |\n" \
+                 "|X X O O X X O |\n" \
+                 "|O O X X O O X |\n" \
+                 "|O O X X O X O |\n" \
+                 "|==============|\n" \
+                 "|0 1 2 3 4 5 6 |"
+
+    assert check_end_state(string_to_board(board_draw), PLAYER1) == GameState.IS_DRAW
+    assert check_end_state(string_to_board(board_draw), PLAYER2,
+                           PlayerAction(4)) == GameState.IS_DRAW
+    assert check_end_state(string_to_board(board1), PLAYER1) == GameState.STILL_PLAYING
+    assert check_end_state(string_to_board(board1), PLAYER2) == GameState.STILL_PLAYING
+    assert check_end_state(string_to_board(board2), PLAYER1) == GameState.IS_WIN
+    assert check_end_state(string_to_board(board3), PLAYER2) == GameState.IS_WIN
+
+    assert check_end_state(string_to_board(board2), PLAYER1,
+                           PlayerAction(2)) == GameState.IS_WIN
+    assert check_end_state(string_to_board(board2), PLAYER1,
+                           PlayerAction(3)) == GameState.STILL_PLAYING
+
+    assert check_end_state(string_to_board(board3), PLAYER2) == GameState.IS_WIN
