20c6780bc4399f52ed3aa6a92d6c2b9ecdc41950
Nico Kestel <=>
2020-11-28 10:50:49 +0100

    adapted minimax tests to alphabeta pruning

--- tests/test_agents/test_agent_minimax/test_minimax.py
+++ tests/test_agents/test_agent_minimax/test_minimax.py
@@ -1,6 +1,9 @@
 def test_minimax():
-    from agents.agent_minimax.minimax import minimax
-    from agents.common import initialize_game_state, string_to_board, PLAYER1, PLAYER2
+    from agents.agent_minimax.minimax import minimax, minimax_ab
+    from agents.common import initialize_game_state, string_to_board, PLAYER1, PLAYER2, PlayerAction
+    import numpy as np
+
+    MIN_VALUE, MAX_VALUE = np.iinfo(np.int32).min, np.iinfo(np.int32).max
 
     board_init = initialize_game_state()
 
@@ -24,28 +27,35 @@ def test_minimax():
             "|==============|\n" \
             "|0 1 2 3 4 5 6 |"
 
-    assert minimax(board=board_init, player=PLAYER1, depth=4)[0] == 3
+    assert minimax(board=board_init, player=PLAYER1, depth=4)[0] == PlayerAction(3)
+    assert minimax_ab(board=board_init, player=PLAYER1, depth=4, a=MIN_VALUE, b=MAX_VALUE)[0] == PlayerAction(3)
 
     # PLAYER1 plays 3 to win, PLAYER2 needs to prevent that
-    assert minimax(board=string_to_board(board_xwin), player=PLAYER1, depth=2)[0] == 3
-    assert minimax(board=string_to_board(board_xwin), player=PLAYER2, depth=2)[0] == 3
+    assert minimax(board=string_to_board(board_xwin), player=PLAYER1, depth=2)[0] == PlayerAction(3)
+    assert minimax(board=string_to_board(board_xwin), player=PLAYER2, depth=2)[0] == PlayerAction(3)
+    assert minimax_ab(board=string_to_board(board_xwin), player=PLAYER1, depth=2, a=MIN_VALUE, b=MAX_VALUE)[
+               0] == PlayerAction(3)
+    assert minimax_ab(board=string_to_board(board_xwin), player=PLAYER2, depth=2, a=MIN_VALUE, b=MAX_VALUE)[
+               0] == PlayerAction(3)
 
     # PLAYER1 wins in 2 turns
-    assert minimax(board=string_to_board(board), player=PLAYER1, depth=4)[0] == 2
+    assert minimax(board=string_to_board(board), player=PLAYER1, depth=4)[0] == PlayerAction(2)
+    assert minimax_ab(board=string_to_board(board), player=PLAYER1, depth=4, a=MIN_VALUE, b=MAX_VALUE)[
+               0] == PlayerAction(2)
 
 
 def test_score():
     from agents.agent_minimax.minimax import score
     from agents.common import string_to_board, PLAYER1, PLAYER2
     board = "|==============|\n" \
-             "|              |\n" \
-             "|              |\n" \
-             "|    X X       |\n" \
-             "|    O X X     |\n" \
-             "|  O X O O     |\n" \
-             "|  O O X X     |\n" \
-             "|==============|\n" \
-             "|0 1 2 3 4 5 6 |"
+            "|              |\n" \
+            "|              |\n" \
+            "|    X X       |\n" \
+            "|    O X X     |\n" \
+            "|  O X O O     |\n" \
+            "|  O O X X     |\n" \
+            "|==============|\n" \
+            "|0 1 2 3 4 5 6 |"
 
     board_draw = "|==============|\n" \
                  "|O O O X O O O |\n" \
