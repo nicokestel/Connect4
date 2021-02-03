59a56335e77f930343e3aa27993188f6d21a5528
Nico Kestel <=>
2021-02-03 17:02:16 +0100

    new random agent

--- agents/agent_random/random.py
+++ agents/agent_random/random.py
@@ -4,7 +4,7 @@ from typing import Tuple, Optional
 
 
 def generate_move_random(
-        board: np.ndarray, player: BoardPiece, saved_state: Optional[SavedState]
+        board: np.ndarray, player: BoardPiece, saved_state: Optional[SavedState], uniform_distribution: bool = False
 ) -> Tuple[PlayerAction, Optional[SavedState]]:
     """
     Generates a random possible move for player on board.
@@ -17,6 +17,9 @@ def generate_move_random(
         The player that needs to play a move
     saved_state : Optional[SavedState]
         ???
+    uniform_distribution : bool
+        True: Moves are equally likely
+        False (default): Moves tend to center column
 
     Returns
     -------
@@ -28,6 +31,15 @@ def generate_move_random(
     """
 
     # Choose a valid, non-full column randomly and return it as 'action'
-    action = np.random.choice(get_non_full_columns(board))
+    if uniform_distribution:
+        return np.random.choice(get_non_full_columns(board)), saved_state
+
+    p = np.array([1/16, 1/8, 3/16, 1/4, 3/16, 1/8, 1/16])
+    available = np.isin(np.arange(0, 7), get_non_full_columns(board))
+
+    p *= available
+    p /= sum(p)
+
+    action = np.random.choice(np.arange(0, 7), p=p)
 
     return action, saved_state
